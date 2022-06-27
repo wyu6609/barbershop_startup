b436283aef4b4e8fafccd58fad157323e61f9479
William Yu <88666309+wyu6609@users.noreply.github.com>
2022-06-27 12:39:14 -0400

    brand navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function OffcanvasExample() {
     <>
       <Navbar bg="light" expand="md" className="mb-3">
         <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
+          <Navbar.Brand href="#">Brand</Navbar.Brand>
           <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-false`} />
           <Navbar.Offcanvas
             id={`offcanvasNavbar-expand-false`}
